8a5550c6d733e6c6b4bbda3d83a1689647463c10
ClaraPons <claragapons@gmail.com>
2022-08-01 17:55:15 +0200

    types

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
-function App() {
+const App = () => {
+
+  const [pokemon, setPokemon] = useState(null)
+
+  useEffect(() => {
+    fetchData()
+    }, [])
+
+  const fetchData = async () => {
+      const request = await fetch("https://pokeapi.co/api/v2/pokemon/1")
+      const response = await request.json()
+      setPokemon(response)
+  }
+
+  if(!pokemon){
+    return <div>chargement...</div>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={pokemon.sprites.front_shiny}></img>
+      <h1>{pokemon.name}</h1>
+      <p>Height : {pokemon.height}</p>
+      <p>Weight : {pokemon.weight}</p>
     </div>
   );
 }
